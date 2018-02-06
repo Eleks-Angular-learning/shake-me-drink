4850037c4462b5468f0ee6f44430f319e2bbeac3
Volodymyr Shevchuk <dosandkv@gmail.com>
2018-02-06 16:42:57 +0200

    #0000 Added basic structure;

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,15 +2,21 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SearchComponent } from './search/search.component';
+import {MainPageComponent} from './main-page/main-page.component';
+import {MakeCocktailComponent} from './make-cocktail/make-cocktail.component';
 
 const routes: Routes = [
   {
-    path: 'login',
-    component: LoginComponent
+    path: '',
+    component: MainPageComponent,
+    children: [
+      {path: 'search', component: SearchComponent},
+      {path: 'make-cocktail', component: MakeCocktailComponent}
+    ]
   },
   {
-    path: 'search',
-    component: SearchComponent
+    path: 'login',
+    component: LoginComponent
   }
 ];
 
---
+++ src/app/make-cocktail/make-cocktail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-make-cocktail',
+  templateUrl: './make-cocktail.component.html',
+  styleUrls: ['./make-cocktail.component.css']
+})
+export class MakeCocktailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
