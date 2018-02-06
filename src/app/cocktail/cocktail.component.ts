336a018fc781beb4eecc44346d5c2cc1c1f2c0e6
Nazar Tsyhanyuk <Nazartsyhanyuk@gmail.com>
2018-02-06 20:28:47 +0200

    change scrollbar and results container styles, add font

--- src/app/cocktail/cocktail.component.ts
+++ src/app/cocktail/cocktail.component.ts
@@ -1,10 +1,15 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-cocktail',
   templateUrl: './cocktail.component.html',
-  styleUrls: ['./cocktail.component.css']
+  styleUrls: ['./cocktail.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class CocktailComponent {
   @Input() cocktail;
+
+getImageUrl (cocktail) {
+  return `http://${cocktail.strDrinkThumb}`;
+}
 }
