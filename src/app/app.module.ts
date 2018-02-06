918154f12b7f0c1bf330326a4097bc8d4a7e8fbf
Nazar Tsyhanyuk <Nazartsyhanyuk@gmail.com>
2018-02-06 15:11:44 +0200

    implement search input and filtering cocktails (#12)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 import { CardModule } from 'primeng/card';
 
 import { AppComponent } from './app.component';
@@ -9,6 +10,7 @@ import { LoginComponent } from './login/login.component';
 import { CocktailComponent } from './cocktail/cocktail.component';
 import { SearchComponent } from './search/search.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FilterCocktailsPipe } from '../pipes/filter-cocktails.pipe';
 
 @NgModule({
   declarations: [
@@ -16,12 +18,14 @@ import { NavBarComponent } from './nav-bar/nav-bar.component';
     LoginComponent,
     CocktailComponent,
     SearchComponent,
-    NavBarComponent
+    NavBarComponent,
+    FilterCocktailsPipe
   ],
   imports: [
     AppRoutingModule,
     BrowserModule,
     HttpClientModule,
+    FormsModule,
     CardModule
   ],
   providers: [],
