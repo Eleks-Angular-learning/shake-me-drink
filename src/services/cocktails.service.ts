be44fa0e7db9bea18dd4ca41d99939aa4009d778
Nazar Tsyhanyuk <Nazartsyhanyuk@gmail.com>
2018-02-07 13:50:37 +0200

    change css to scss, change map to pluck in cocktails service

--- src/services/cocktails.service.ts
+++ src/services/cocktails.service.ts
@@ -1,15 +1,16 @@
 
-import { HttpClient, HttpResponse  } from '@angular/common/http';
+import { HttpClient  } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { pluck } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
 import { CocktailsList } from '../app/app.models';
-import 'rxjs/add/operator/map';
 
 @Injectable()
 export class CocktailsService {
   constructor (private http: HttpClient) {}
 
-  getCocktails() {
-    return this.http.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail')
-      .map((res: HttpResponse<{drinks: CocktailsList}>) => res['drinks']);
+  getCocktails(): Observable<CocktailsList> {
+    const response = this.http.get('http://www.thecocktaildb.com/api/json/v1/1/filter.php?c=Cocktail');
+    return response.pipe(pluck('drinks'));
   }
 }
